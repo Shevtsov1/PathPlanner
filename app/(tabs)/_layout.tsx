a36b012b65e98590c76d0da9149de517b412e365
v.shautsou <shavec3@gmail.com>
2025-09-30 14:44:08 +0300

    Add new TabBarIcons from Ionicons and const for size

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,6 +5,9 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const tabBarIconSize: number = 24;
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -20,35 +23,35 @@ export default function TabLayout() {
           name="CalendarIntegration"
           options={{
               title: 'Календарь',
-              tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+              tabBarIcon: ({ color }) => <Ionicons name="calendar-clear" size={tabBarIconSize} color={color} />,
           }}
       />
       <Tabs.Screen
           name="TaskPlanner"
           options={{
               title: 'Задачи',
-              tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+              tabBarIcon: ({ color }) => <Ionicons name="layers" size={tabBarIconSize} color={color} />,
           }}
       />
       <Tabs.Screen
         name="Home"
         options={{
           title: 'Главная',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={tabBarIconSize} color={color} />,
         }}
       />
       <Tabs.Screen
           name="IncomeExpense"
           options={{
               title: 'Бюджет',
-              tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+              tabBarIcon: ({ color }) => <Ionicons name="wallet" size={tabBarIconSize} color={color} />,
           }}
       />
       <Tabs.Screen
         name="Profile"
         options={{
           title: 'Профиль',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons name="person" size={tabBarIconSize} color={color} />,
         }}
       />
     </Tabs>
